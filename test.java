a3117adfa8c72f6daa42944c9ba76aab16f45668
Minoru Matsuoka <minoru.matsuoka@ucl.meijo-u.ac.jp>
2018-04-26 14:50:11 +0900

    test.javaの変更
    test.javaの変更

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 public class Text{
   public static void main(String[] args){
-    System.out.printf(Hello, Java!);
+    System.out.printf("Hello, Java!");
+
+    System.out.print("変更のテスト");
   }
 }
